ff42d72ef4d8847109778937a3b7dcdff64a94c3
nrm <favitgirl@gmail.com>
2026-01-06 21:38:20 +0100

    Add files via upload

---
+++ scriptSQL/test_increment.sql
@@ -0,0 +1,11 @@
+use northwind;
+
+--This is to test the incremental loading 
+
+INSERT INTO CustomerDemographics (CustomerTypeID, CustomerDesc)
+VALUES ('VIP', 'Very Important Customer'), 
+       ('REG', 'Regular Customer');
+GO
+INSERT INTO CustomerCustomerDemo (CustomerID, CustomerTypeID)
+VALUES ('ALFKI', 'VIP'),
+       ('ANATR', 'REG');
